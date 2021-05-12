bce6ceb6f0a8f6ff3aaea288aa87c687e69f6d95
lihbr <lihbr@users.noreply.github.com>
2021-05-12 16:55:57 +0200

    fix(document): alternate language typing

--- src/document.ts
+++ src/document.ts
@@ -1,5 +1,12 @@
 import { AnyRegularField, GroupField, SliceZone } from "./fields";
 
+interface AlternateLanguage {
+	id: string;
+	uid?: string;
+	type: string;
+	lang: string;
+}
+
 export interface PrismicDocumentHeader<TypeEnum = string, LangEnum = string> {
 	id: string;
 	uid: string | null;
@@ -12,7 +19,7 @@ export interface PrismicDocumentHeader<TypeEnum = string, LangEnum = string> {
 	slugs: string[];
 	linked_documents: unknown[]; // TODO: Not sure of the type for this one
 	lang: LangEnum;
-	alternate_languages: LangEnum[]; // TODO: Check if it's actually that
+	alternate_languages: AlternateLanguage[];
 }
 
 export interface PrismicDocument<
