791ba34f0b002a9dc5d90518a93d06d18c636742
Nick DeJesus <nick@Nicks-MacBook-Air.local>
2021-08-03 09:45:09 -0400

    adding comments to document.ts

--- src/document.ts
+++ src/document.ts
@@ -1,12 +1,17 @@
 import { AnyRegularField, GroupField, SliceZone } from "./fields";
 
+/**
+ * Alternate Language to query by
+ */
 interface AlternateLanguage {
 	id: string;
 	uid?: string;
 	type: string;
 	lang: string;
 }
-
+/**
+ * Metadata for Prismic Document
+ */
 export interface PrismicDocumentHeader<TypeEnum = string, LangEnum = string> {
 	id: string;
 	uid: string | null;
@@ -21,7 +26,11 @@ export interface PrismicDocumentHeader<TypeEnum = string, LangEnum = string> {
 	lang: LangEnum;
 	alternate_languages: AlternateLanguage[];
 }
-
+/**
+ * Prismic Documents are instances of CustomTypes.
+ * 
+ * More details on Custom Types: {@link https://prismic.io/docs/core-concepts/custom-types}
+ */
 export interface PrismicDocument<
 	DataInterface = Record<string, AnyRegularField | GroupField | SliceZone>,
 	TypeEnum = string,
