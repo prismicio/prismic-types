453328cbec33770b66c4be1043d775a8c926932b
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-08-04 09:58:58 -1000

    feat: add CustomTypeModelDefinition and restructure CustomTypeModel
    The changes mirror the Custom Types API

--- src/customType.ts
+++ src/customType.ts
@@ -6,11 +6,37 @@
 type IterableElement<T> = T extends Iterable<infer U> ? U : never;
 
 /**
- * A Prismic Custom Type model expressed in JSON.
+ * A Prismic Custom Type model.
+ *
+ * @typeParam ID API ID of the Custom Type.
+ * @typeParam Definition The Custom Type's tabs and their fields.
+ */
+export interface CustomTypeModel<
+	ID extends string = string,
+	Definition extends CustomTypeModelDefinition = CustomTypeModelDefinition,
+> {
+	/** The ID of the Custom Type model. */
+	id: ID;
+
+	/** The human readable name of the Custom Type Model. */
+	label: string;
+
+	/** Determines if more than one document for the Custom Type can be created. */
+	repeatable: boolean;
+
+	/** The Custom Type model definition. */
+	json: Definition;
+
+	/** Determines if new documents for the Custom Type can be created. */
+	status: boolean;
+}
+
+/**
+ * A Prismic Custom Type's tabs and their fields.
  *
  * @typeParam TabName Names of Custom Type tabs.
  */
-export type CustomTypeModel<TabName extends string = string> = Record<
+export type CustomTypeModelDefinition<TabName extends string = string> = Record<
 	TabName,
 	CustomTypeModelTab
 >;
@@ -476,10 +502,11 @@ export interface CustomTypeModelSharedSlice {
  * @typeParam Variation A variation for the Shared Slice.
  */
 export interface SharedSliceModel<
+	ID extends string = string,
 	Variation extends SharedSliceModelVariation = SharedSliceModelVariation,
 > {
 	type: CustomTypeModelSliceType.SharedSlice;
-	id: string;
+	id: ID;
 	name: string;
 	description: string;
 	variations: readonly Variation[];
