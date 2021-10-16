3ce7f197bf5c377288693763b0d63e0400633719
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-10-15 17:40:45 -1000

    test: restore stub ava test

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import test from "ava";
+
+import * as prismicT from "../src";
+
+// TODO: Dummy test, meant to be removed when real tests come in
+test("exports something", (t) => {
+	t.truthy(prismicT);
+	t.log(prismicT);
+});
