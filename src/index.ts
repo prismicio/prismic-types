638e424c6cfadd6f489f01d0168712b8db465fd0
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-08-18 16:42:13 -1000

    chore: add note about the `/graphql` entry

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from "./api";
 export * from "./model";
 export * from "./value";
 export * from "./webhook";
+
+// The GraphQL exports are purposely not included in the root-level API.
+// Instead, they are provided under their own `/graphql` entry.
+// export * from "./webhook";
