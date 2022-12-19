f9774a7524be3e09540674eae7eb839e0f969337
lihbr <lihbr@users.noreply.github.com>
2022-12-19 14:35:53 +0100

    build: graphql legacy export

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import sdk from "vite-plugin-sdk";
 
 export default defineConfig({
+	build: {
+		lib: {
+			entry: {
+				index: "./src/index.ts",
+				graphql: "./src/graphql/index.ts",
+			},
+		},
+	},
 	plugins: [sdk()],
 	test: {
 		coverage: {
